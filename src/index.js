4b5cac8bde9ebf59ad542ab361df06bc0783b71c
DenyingTheTruth <anton.bachikin@gmail.com>
2018-09-28 12:09:53 +0300

    function getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let result = 0;
+  
+  while (number > 0){
+    number = number/5;
+    result += Math.floor(number);
+  }
+  return result;
 }
